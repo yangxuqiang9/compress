bc7d611c6b90edc835794ca26d3dc55ea9d392e4
yxq <932477876@qq.com>
2016-08-06 17:35:02 +0800

    添加service

--- app/src/main/java/com/example/a328789/compress/MainActivity.java
+++ app/src/main/java/com/example/a328789/compress/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
         init();
     }
 
